8d8b036ff0f100181abe74041e2ce2d9daf2cfd6
knight-of-design <knight.of.design@gmail.com>
2019-07-13 02:38:50 -0700

    Create Angular Todo App Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
   title = 'angular-todo';
